fa508551e29f3dd79d3c3802669315fc07e52660
kadimakeith <keithkadima@gmail.com>
2023-01-09 16:25:02 +0000

    finish setting up app structure

--- server/express.js
+++ server/express.js
@@ -5,10 +5,12 @@ import compress from 'compression'
 import cors from 'cors'
 import helmet from 'helmet'
 const app = express()
+
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cookieParser())
 app.use(compress())
 app.use(helmet())
 app.use(cors())
+
 export default app
