ae014ea4ec35d43e872898540c0e4d0c8bc9de3f
kadimakeith <keithkadima@gmail.com>
2023-01-10 12:41:46 +0300

    update config.js

--- config/config.js
+++ config/config.js
@@ -3,9 +3,6 @@ const config = {
     port: process.env.PORT || 3000,
     jwtSecret: process.env.JWT_SECRET || "YOUR_secret_key",
     mongoUri: process.env.MONGODB_URI ||
-    process.env.MONGO_HOST ||
-    'mongodb://' + (process.env.IP || 'localhost') + ':' +
-    (process.env.MONGO_PORT || '27017') +
-    '/mernproject'
+    process.env.MONGO_HOST || 'mongodb://' + (process.env.IP || 'localhost') + ':' + (process.env.MONGO_PORT || '27017') + '/mernproject'
    }
    export default config
